df6efa2cbebd4de0a6d218b35c02ad212be50ff2
Jeff. <mrjeffersonx@gmail.com>
2026-02-24 23:59:42 +0100

    Update shelly-ai to v1.1.0

--- Formula/shelly-ai.rb
+++ Formula/shelly-ai.rb
@@ -1,26 +1,26 @@
 class ShellyAi < Formula
   desc "AI-powered terminal assistant that converts natural language to shell commands"
   homepage "https://github.com/Jeff909Dev/shelly"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Jeff909Dev/shelly/releases/download/v1.0.0/shelly-ai-darwin-arm64.tar.gz"
-      sha256 "161f023428528175652a612c8318215ff3a7c7a32aaa57b296568f159f43a640"
+      url "https://github.com/Jeff909Dev/shelly/releases/download/v1.1.0/shelly-ai-darwin-arm64.tar.gz"
+      sha256 "8efc7698d4ca799f24c39b3a2c1ac9865a523291c8a2a393d0e0af181005e52b"
     else
-      url "https://github.com/Jeff909Dev/shelly/releases/download/v1.0.0/shelly-ai-darwin-amd64.tar.gz"
-      sha256 "edb755a3992786ac2480c5730f5b841a32a8f24e56e98f1ea9757a7d3df95994"
+      url "https://github.com/Jeff909Dev/shelly/releases/download/v1.1.0/shelly-ai-darwin-amd64.tar.gz"
+      sha256 "9cb305d6c44a932658769d1de401908e45896b9922a65260ca2b97fe5d9d2909"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Jeff909Dev/shelly/releases/download/v1.0.0/shelly-ai-linux-arm64.tar.gz"
-      sha256 "a9b8f3eb374b457759edfeb5d2ca12595e68fa1015fb82bea5563bedd75e5bcf"
+      url "https://github.com/Jeff909Dev/shelly/releases/download/v1.1.0/shelly-ai-linux-arm64.tar.gz"
+      sha256 "937ba7c0f1b93fd9c67b4cbd83f70ec5c810933b3de224b460663a47bc41767b"
     else
-      url "https://github.com/Jeff909Dev/shelly/releases/download/v1.0.0/shelly-ai-linux-amd64.tar.gz"
-      sha256 "9017764ed52a94d1e15785f9c897b37904bbca03b5d6c331dc10f40dec09835d"
+      url "https://github.com/Jeff909Dev/shelly/releases/download/v1.1.0/shelly-ai-linux-amd64.tar.gz"
+      sha256 "49fe24831a10e37c1f062669e891d0f2006a6cbadfd7f8787bfe06c6042c479b"
     end
   end
 
